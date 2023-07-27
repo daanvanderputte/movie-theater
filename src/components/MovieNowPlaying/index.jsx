873238ab62981e197bad639ed102b4e4dd4e90a8
daanvanderputte <daanvanderputte@protonmail.com>
2023-07-27 13:05:41 +0200

    created functional component MovieNowPlaying

---
+++ src/components/MovieNowPlaying/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./index.css";
+
+const MovieNowPlaying = () => {
+  const [movieNowPlaying, setMovieNowPlaying] = useState([]);
+
+  const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}`;
+
+  useEffect(() => {
+    const fetchMovieNowPlaying = async () => {
+      try {
+        const response = await axios.get(url);
+        setMovieNowPlaying(response.data.results);
+      } catch (error) {
+        console.error("Error fetching movies now playing:", error);
+      }
+    };
+
+    fetchMovieNowPlaying();
+  }, []);
+
+  return (
+    <div>
+      <div>Movies</div>
+      <div className="movie-cards-container">
+        {movieNowPlaying.map((movie) => (
+          <div key={movie.id} className="movie-card">
+            <div className="movie-image">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+              />
+            </div>
+            <div className="movie-title">{movie.title}</div>
+            <div className="movie-release-date">{movie.release_date}</div>
+            <div className="movie-overview">{movie.overview}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MovieNowPlaying;
