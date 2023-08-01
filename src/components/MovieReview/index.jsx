6825387cb28e37f397dede2a6ef6fedb4bd6620a
daanvanderputte <daanvanderputte@protonmail.com>
2023-08-01 15:04:02 +0200

    changed fetching reviews and added fetching movie details

--- src/components/MovieReview/index.jsx
+++ src/components/MovieReview/index.jsx
@@ -4,12 +4,23 @@ import axios from "axios";
 import "./index.css";
 
 const MovieReview = () => {
+  const [movieDetail, setMovieDetail] = useState({});
   const [movieReview, setMovieReview] = useState([]);
   const { movieId } = useParams();
 
-  const url = `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${process.env.REACT_APP_API_KEY}`;
+  const detailUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.REACT_APP_API_KEY}`;
+  const reviewUrl = `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${process.env.REACT_APP_API_KEY}`;
 
   useEffect(() => {
+    const fetchMovieDetail = async () => {
+      try {
+        const response = await axios.get(detailUrl);
+        setMovieDetail(response.data);
+      } catch (error) {
+        console.error("Error fetching movie details:", error);
+      }
+    };
+
     const fetchMovieReview = async () => {
       try {
         const response = await axios.get(url);
