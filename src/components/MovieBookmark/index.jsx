1dd6943fedbedaf3039ff52da5d5bd7b3877e8ee
daanvanderputte <daanvanderputte@protonmail.com>
2023-08-01 12:52:16 +0200

    created functional component MovieBookmark

---
+++ src/components/MovieBookmark/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const MovieBookmark = ({
+  movieBookmark,
+  movieNowPlaying,
+  handleMovieCardClick,
+}) => {
+  const getMovieBookmarkById = (movieId) => {
+    return movieNowPlaying.find((movie) => movie.id === movieId);
+  };
+
+  return (
+    <div>
+      <div className="title">Bookmarked Movies</div>
+      <div className="movie-cards-container">
+        {movieBookmark.map((movieId) => {
+          const movie = getMovieBookmarkById(movieId);
+          if (movie) {
+            return (
+              <div
+                key={movie.id}
+                className="movie-card"
+                onClick={() => handleMovieCardClick(movie.id)}
+              >
+                <div className="movie-image">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.title}
+                  />
+                </div>
+                <div className="movie-title">{movie.title}</div>
+                <div className="movie-release-date">
+                  {formatedDate(movie.release_date)}
+                </div>
+                <div className="movie-overview">{movie.overview}</div>
+              </div>
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MovieBookmark;
